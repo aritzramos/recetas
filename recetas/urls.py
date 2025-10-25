8a9fe4eae7c3624fab2395a2f483c26fd93411c0
aritzramos <aritz.ramos.lozares.al@iespoligonosur.org>
2025-10-25 12:04:29 +0200

    url5 hecha

--- recetas/urls.py
+++ recetas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('recipes/url2/<int:year_recipe>/<int:month_recipe>', views.get_recipe_date,name='get_recipe_date'),
     path('recipes/url3/<str:theme>/', views.get_user_theme, name="get_user_theme"),
     path('category/<str:description>', views.get_category_recipe, name='get_category_recipe'),
+    path('last-user-comment/<int:recipe>', views.get_last_user_recipe, name='get_last_user_recipe'),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -8,15 +8,14 @@ def index(request):
 
 
 def list_recipes(request):
-    recipes=Recipe.objects.select_related("author").prefetch_related("recipe_ingredient", "category")
-    recipes = recipes.all()
+    recipes=Recipe.objects.select_related("author").prefetch_related("recipe_ingredient__ingredient", "category").all()
     #recipes = (Recipe.objects.raw("SELECT * FROM recetas_recipe r "
      #                             + " JOIN recetas_user u ON r.user_id = u.id "
       #                            + " JOIN recetas_ingredient i ON i.ingredient_id = r.id "))
     return render(request, 'recipe/list.html',{"recipes_list":recipes})
 
 def get_recipe_date(request, year_recipe, month_recipe):
-    recipes=Recipe.objects.select_related("author").prefetch_related("recipe_ingredient", "category")
+    recipes=Recipe.objects.select_related("author").prefetch_related("recipe_ingredient", "category").all()
     recipes = recipes.all()
     recipes = recipes.filter(created__year=year_recipe, created__month=month_recipe)
     return render(request, 'recipe/url2.html',{"recipes_list":recipes})
@@ -30,3 +29,9 @@ def get_category_recipe(request, description):
     recipe = Recipe.objects.prefetch_related("category")
     recipe = recipe.filter(category__description__icontains=description)
     return render(request, 'recipe/url4.html', {'get_text':recipe})
+
+
+#Esto tiene que ser ultimo usuario en comentar una receta.
+def get_last_user_recipe(request, recipe):
+    comment = Comment.objects.filter(recipe=recipe).select_related("author", "recipe").order_by('-created_at').first()
+    return render(request, 'user/url5.html',{"comment":comment})
