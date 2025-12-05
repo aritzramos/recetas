4d4d6c2264f62e4301c0868aab081654a9b81790
aritzramos <aritz.ramos.lozares.al@iespoligonosur.org>
2025-12-05 10:56:40 +0100

    Modal metido

---
+++ recetas/static/javascript/recetas.js
@@ -0,0 +1,6 @@
+const btnDelete  = document.getElementById("btnDelete")
+
+btnDelete.addEventListener("click", ()  => {
+    const formDelete = document.getElementById("form");
+    formDelete.submit();
+});
